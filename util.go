2f6a89cdd738e972fe0c42a7cfc209472cf63632
Hideo Hattori <hhatto.jp@gmail.com>
2024-06-02 14:41:22 +0900

    fix: NaN

--- util.go
+++ util.go
@@ -5,7 +5,14 @@ import (
 	"math"
 )
 
-func getPercentageValue(value float64, precision int) string {
+func getPercentageValue(numerator, denominator uint, precision int) string {
+	var value float64
+	if denominator == 0 {
+		value = 0.0
+	} else {
+		value = float64(numerator) / float64(denominator) * 100
+	}
+
 	if precision <= 0 {
 		return fmt.Sprintf("%.0f", math.Round(value))
 	}
